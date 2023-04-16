45512989db681fd9aac76c08b2d3883312d7d032
BenjaminJacobson <ben.jacobson@flatironschool.com>
2023-04-16 14:36:49 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function appendCat(name) {
+    const copyCats = [...cats, name];
+    return copyCats
+}
+
+function prependCat(name) {
+    const copyCats = [name, ...cats];
+    return copyCats
+}
+
+function removeFirstCat () {
+    return cats.slice(1);
+  }
+  
+  function removeLastCat () {
+    return cats.slice(0, cats.length - 1);
+  }
